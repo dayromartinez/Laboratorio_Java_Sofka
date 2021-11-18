e4db55a83498f9f030a6cf15141b044418d1ddeb
Dayro Martínez <djmd96@hotmail.com>
2021-11-17 23:01:47 -0500

    Ejercicio 13 resuelto

---
+++ src/main/java/Ejercicio13/Main.java
@@ -0,0 +1,16 @@
+package Ejercicio13;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date fechaActual = Calendar.getInstance().getTime();
+        DateFormat formateador = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss a");
+        String fechaCadena = formateador.format(fechaActual);
+        System.out.println("La fecha y hora actual es: "+fechaCadena);
+    }
+}
